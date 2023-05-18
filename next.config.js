ed5af0265c9ff5e82ba64ba4673d02ef28841a7d
jonra1993 <jon_ra@hotmail.es>
2023-05-18 10:20:23 -0500

    Start project

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      runtime: 'edge',
+    },
+    reactStrictMode: true,
+  }
+module.exports = nextConfig
