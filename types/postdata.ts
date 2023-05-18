653d31610c7e7e13ce1936802030f090b663201c
jonra1993 <jon_ra@hotmail.es>
2023-05-18 11:15:50 -0500

    Add dynamic paths

---
+++ types/postdata.ts
@@ -0,0 +1,15 @@
+export interface PostData {
+    userId: number
+    id: number
+    title: string
+    body: string
+  }
+  
+  export interface PostDataProps {
+    postData: PostData
+  }
+  
+  export interface PostDataListProps {
+    postDataList: PostData[]
+  }
+  
