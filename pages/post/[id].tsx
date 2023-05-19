6f7e600293bc8a205a19954c737d8146847a88ae
jonra1993 <jon_ra@hotmail.es>
2023-05-19 18:40:10 -0500

    Edit

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,11 +1,19 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { GetPost } from '../../lib/postdata_api'
 import { PostData } from '../../types/postdata'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
+import Test from '../../components/test'
 
-const Post = () => {
+interface IPost {
+  source: any
+}
+
+const Post:  FC<IPost> = (props)  => {
+  const { source } = props
   const [postData, setPostData] = useState<null | PostData>(null)
   const router = useRouter()
   const id = router.query.id as string
@@ -15,7 +23,7 @@ const Post = () => {
     GetPost(id).then(r => setPostData(r))
   }, [router.isReady, id])
 
-  if (!postData) return 'Loading...'
+  if (!postData) return <>'Loading...'</>
 
   return (
     <main>
@@ -26,12 +34,20 @@ const Post = () => {
       <h1>{postData.title}</h1>
 
       <p>{postData.body}</p>
+      <MDXRemote {...source} components={components} />
 
       <Link href="/">
         Go back to home
       </Link>
     </main>
   )
+}
+
+export async function getStaticProps() {
+  // MDX text - can be from a local file, database, anywhere
+  const source = 'Some **mdx** text, with a component <Test />'
+  const mdxSource = await serialize(source)
+  return { props: { source: mdxSource } }
 }
 
 export default Post
